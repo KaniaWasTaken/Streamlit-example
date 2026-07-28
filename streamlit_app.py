3b39d703d86fc66f1ef5891f8ea1ac1a5054ed00
KaniaWasTaken <kaniawastaken@gmail.com>
2026-07-28 17:01:34 +0530

    Fetch and display data on button click

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ with st.form(key="ticker-form"):
 
   btn = st.form_submit_button()
 
-
-df = yf.download(ticker,period="5y")
-st.table(df.head())
-st.line_chart(df["Close"])
+if btn:
+  df = yf.download(ticker,period="5y")
+  st.table(df.head())
+  st.line_chart(df["Close"])
