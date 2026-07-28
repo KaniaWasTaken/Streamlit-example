0c7d6675c0fa4784d018928cbd1712e5f59773a3
KaniaWasTaken <kaniawastaken@gmail.com>
2026-07-28 16:54:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import pandas as pd
 import yfinance as yf
 
 st.write("Hello")
-df = yf.download("GOOGL",period="5y",interval="1m")
+df = yf.download("GOOGL",period="5y")
 st.table(df.head())
 st.line_chart(df["Close"])
