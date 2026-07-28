beeec488e69cdebf2957a03cd09cf05de1ae811c
KaniaWasTaken <kaniawastaken@gmail.com>
2026-07-28 17:24:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,12 @@ import yfinance as yf
 
 with st.form(key="ticker-form"):
   ticker = st.text_input("Enter ticker")
-
-  btn = st.form_submit_button()
+  btn = st.form_submit_button("Search")
 
 if btn:
   df = yf.download(ticker,period="5y")
-  st.table(df.head())
-  st.line_chart(df["Close"])
+  if df.empty:
+    st.error("Invalid Ticker")
+  else:
+    st.table(df.head())
+    st.line_chart(df["Close"])
