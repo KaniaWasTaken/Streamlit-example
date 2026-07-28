d69ae6149464bc985bf96148f7b7a60a6a29f475
KaniaWasTaken <kaniawastaken@gmail.com>
2026-07-28 17:00:09 +0530

    Replace static ticker with user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ with st.form(key="ticker-form"):
 
   btn = st.form_submit_button()
 
-st.write("Hello")
-df = yf.download("GOOGL",period="5y")
+
+df = yf.download(ticker,period="5y")
 st.table(df.head())
 st.line_chart(df["Close"])
