673a1a10a0b00baec2e1dab982ecabea31d490b5
KaniaWasTaken <kaniawastaken@gmail.com>
2026-07-28 16:59:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import yfinance as yf
 with st.form(key="ticker-form"):
   ticker = st.text_input("Enter ticker")
 
-  btn = st.form_sumit_button()
+  btn = st.form_submit_button()
 
 st.write("Hello")
 df = yf.download("GOOGL",period="5y")
