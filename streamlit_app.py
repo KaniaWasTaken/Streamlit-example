0ca589d043ecfb66246eaa8f8ba1b06bdac17b5e
KaniaWasTaken <kaniawastaken@gmail.com>
2026-07-28 17:58:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,7 +97,7 @@ if submit:
     latest = X.iloc[[-1]]
 
     tomorrow_price = model.predict(latest)[0]
-    current_price = df["Close"].iloc[-2]
+    current_price = float(df["Close"].iloc[-1])
 
     st.subheader("Prediction")
 
